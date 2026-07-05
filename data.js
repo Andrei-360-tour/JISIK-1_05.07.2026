037f3f0d96306d8bcb2a22100fd5db088ab9806c
Andrei-360-tour <156935416+Andrei-360-tour@users.noreply.github.com>
2026-07-05 16:57:13 +0300

    Commit

---
+++ data.js
@@ -0,0 +1,143 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-overview1",
+      "name": "OVERVIEW1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.0318883457521153,
+        "pitch": 0.16678074291208667,
+        "fov": 1.3412181875493232
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.005510430253623966,
+          "pitch": 0.1934641708885252,
+          "rotation": 0,
+          "target": "1-overview2"
+        },
+        {
+          "yaw": 1.6598955705584064,
+          "pitch": 0.17210682963722945,
+          "rotation": 0,
+          "target": "2-overview3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-overview2",
+      "name": "OVERVIEW2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.1358207319071294,
+        "pitch": 0.26090494085296,
+        "fov": 1.3412181875493232
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.11615191682470893,
+          "pitch": 0.17185948881158453,
+          "rotation": 0,
+          "target": "0-overview1"
+        },
+        {
+          "yaw": -0.9572594828556618,
+          "pitch": 0.13436048268221157,
+          "rotation": 0,
+          "target": "2-overview3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-overview3",
+      "name": "OVERVIEW3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.7146901096226035,
+        "pitch": 0.24086363671167454,
+        "fov": 1.3412181875493232
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0982855795516357,
+          "pitch": 0.18968658265919913,
+          "rotation": 0,
+          "target": "0-overview1"
+        },
+        {
+          "yaw": 2.3387197245484073,
+          "pitch": 0.15811684257088743,
+          "rotation": 0,
+          "target": "1-overview2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "JISIK-1_05.07.2026",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
